8cd342e06adc6e7f7d9cedf73b9281747ef2757b
injkgz <injecti0n.kg@gmail.com>
2020-08-20 17:02:45 +0700

    feat: usersmodule init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
-import { Users } from './users/users';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "./users/users.module";
+import { Users } from "./users/users.entity";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mongodb',
-      host: 'localhost',
-      database: 'test',
+      type: "mongodb",
+      host: "localhost",
+      database: "test",
       entities: [Users],
       synchronize: true,
     }),
