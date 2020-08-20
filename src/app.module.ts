8127fa77df4880b952dbeaa8c9285e2d9f70a44b
injkgz <injecti0n.kg@gmail.com>
2020-08-20 16:45:28 +0700

    init commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { Users } from './users/users';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'localhost',
+      database: 'test',
+      entities: [Users],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
