cce402fc99041df47217d401488a10d9d38e8e50
injkgz <injecti0n.kg@gmail.com>
2020-09-01 18:40:39 +0700

    added external rule repository

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -3,14 +3,16 @@ import { Group } from './entity/group.entity';
 import { CreateGroupDto } from './input/create-group.input';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Engine } from 'json-rules-engine';
+import { request, gql } from 'graphql-request';
 
 @Injectable()
 export class GroupService {
+  private ruleEngine = new Engine();
   constructor(
     @InjectModel(Group.name)
     private readonly groupModel: Model<Group>,
   ) {}
-
   async findAll(): Promise<Group[]> {
     return this.groupModel.find();
   }
@@ -25,14 +27,34 @@ export class GroupService {
   }
 
   async create(createGroupDto: CreateGroupDto): Promise<Group> {
-    const group = await this.groupModel.findOne({
-      title: createGroupDto.title,
+    const query = gql`
+      {
+        rules(type: "group")
+      }
+    `;
+    let { rules } = await request('http://localhost:3001/graphql', query);
+    console.log(rules);
+    rules = JSON.parse(rules);
+    this.ruleEngine.addRule(rules);
+    const fact = { group: createGroupDto };
+    const results = await this.ruleEngine.run(fact).catch(err => {
+      throw new HttpException(err, 500);
     });
+    if (!results.events.length) {
+      throw new HttpException(
+        "It's unable to create group with this title",
+        500,
+      );
+    } else {
+      const group = await this.groupModel.findOne({
+        title: createGroupDto.title,
+      });
 
-    if (group) {
-      throw new HttpException('Group already exits', 500);
+      if (group) {
+        throw new HttpException('Group already exits', 500);
+      }
+
+      return this.groupModel.create(createGroupDto);
     }
-
-    return this.groupModel.create(createGroupDto);
   }
 }
